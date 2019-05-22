78855f32b736b9820a3be5f2cabc2657d57f41a1
root <root@Ubuntu18.04>
2019-05-22 23:52:54 +0800

    添加DM9621网卡支持 1、添加dm9621驱动 2、在配置文件添加支持

--- board/samsung/itop4412/itop4412.c
+++ board/samsung/itop4412/itop4412.c
@@ -21,6 +21,8 @@ u32 get_board_rev(void)
 int exynos_init(void)
 {
 #ifdef CONFIG_CMD_USB
+	/* USB3503A Reference frequency */
+	gpio_request(EXYNOS4X12_GPIO_C01, "DM9621");
 	/* USB3503A Connect */
 	gpio_request(EXYNOS4X12_GPIO_M33, "USB3503A Connect");
 
@@ -32,6 +34,12 @@ int exynos_init(void)
 
 int board_usb_init(int index, enum usb_init_type init)
 {
+	gpio_direction_output(EXYNOS4X12_GPIO_M33, 0);
+	gpio_direction_output(EXYNOS4X12_GPIO_C01, 0);
+	gpio_direction_output(EXYNOS4X12_GPIO_M24, 0);
+	gpio_direction_output(EXYNOS4X12_GPIO_M24, 1);
+	gpio_direction_output(EXYNOS4X12_GPIO_C01, 1);
+	gpio_direction_output(EXYNOS4X12_GPIO_M33, 1);
 	return 0;
 }
 
--- common/usb_hub.c
+++ common/usb_hub.c
@@ -222,10 +222,13 @@ void usb_hub_reset(void)
 	memset(hub_dev, 0, sizeof(hub_dev));
 
 	gpio_direction_output(EXYNOS4X12_GPIO_M33, 0);
+	gpio_direction_output(EXYNOS4X12_GPIO_C01, 0);
 	gpio_direction_output(EXYNOS4X12_GPIO_M24, 0);
 
 	gpio_direction_output(EXYNOS4X12_GPIO_M24, 1);
+	gpio_direction_output(EXYNOS4X12_GPIO_C01, 1);
 	gpio_direction_output(EXYNOS4X12_GPIO_M33, 1);
+
 }
 /*
 static struct usb_hub_device *usb_hub_allocate(void)
